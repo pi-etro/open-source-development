9b13870c7dd08361b592134cc678738876860767
Pietro <pietro.gregorio@usp.br>
2026-03-01 03:02:13 -0300

    feat: initial version of activate.sh

---
+++ lk_dev/activate.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# environment variables
+export LK_DEV_DIR="$HOME/github/open-source-development/lk_dev" # path to testing environment directory
+
+# prompt preamble
+prompt_preamble='(LK-DEV)'
+
+# colors
+GREEN="\e[32m"
+PINK="\e[35m"
+BOLD="\e[1m"
+RESET="\e[0m"
+
+# launch Bash shell session w/ env vars defined
+echo -e "${GREEN}Entering shell session for Linux Kernel Dev${RESET}"
+echo -e "${GREEN}To exit, type 'exit' or press Ctrl+D.${RESET}"
+exec bash --rcfile <(echo "source ~/.bashrc; PS1=\"\[${BOLD}\]\[${PINK}\]${prompt_preamble}\[${RESET}\] \$PS1\"")
