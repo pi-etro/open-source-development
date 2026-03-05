f14f884d5d9d91ba746ae0d4b9e7c9d4a26c39d5
Pietro <pietro.gregorio@usp.br>
2026-03-05 02:30:35 -0300

    feat: configure KW directory

--- lk_dev/activate.sh
+++ lk_dev/activate.sh
@@ -4,6 +4,7 @@
 export LK_DEV_DIR="/home/lk_dev" # path to testing environment directory
 export VM_DIR="${LK_DEV_DIR}/vm" # path to VM directory
 export BOOT_DIR="${VM_DIR}/arm64_boot" # path to boot artifacts
+export KW_DIR="${LK_DEV_DIR}/kw" # path to `kw` repository
 
 # utility functions
 
